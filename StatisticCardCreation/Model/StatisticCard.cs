2cc38a4072f4bf1d7f470d563d617da58be9561c
HydbergEugi <57098807+HydbergEugi@users.noreply.github.com>
2021-02-01 10:50:20 +0300

    Добавьте файлы проекта.

---
+++ StatisticCardCreation/Model/StatisticCard.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticCardCreation.Model
+{
+    class StatisticCard
+    {
+    }
+}
---
+++ StatisticCardCreation/ViewModel/StatisticCardModelView.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticCardCreation.ViewModel
+{
+    class StatisticCardModelView
+    {
+    }
+}
